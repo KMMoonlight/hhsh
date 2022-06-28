c98eb1b440800b86e2e10750808b0b94b6151c18
sai <kmmoonlight@outlook.com>
2022-06-29 00:48:01 +0800

    xxx

---
+++ api/proxy.js
@@ -0,0 +1,18 @@
+const  { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+    let target = ''
+
+    if (req.url.startsWith('/api')) {
+        target = 'https://lab.magiconch.com'
+    }
+
+
+    createProxyMiddleware({
+        target,
+        changeOrigin: true,
+        pathRewrite: {
+            '^/api/': '/'
+        }
+    })(req, res)
+}
